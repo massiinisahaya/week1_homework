9e59f01ddcc26ac0b40ad60e14ad020467c6e783
massiinisahaya <biwalake2018@gmail.com>
2024-12-02 22:03:25 +0900

    week1_homework first correction

--- a-8.py
+++ a-8.py
@@ -1,6 +1,5 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
 # ここにコードを記述
-print(f"Name: {users_info[0][0]}, Age: {users_info[0][1]}")
-print(f"Name: {users_info[1][0]}, Age: {users_info[1][1]}")
-print(f"Name: {users_info[2][0]}, Age: {users_info[2][1]}")
+for i in range(3):
+    print(f"Name: {users_info[i][0]}, Age: {users_info[i][1]}")
