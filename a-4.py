db2e30d1be2fd1f89af21d41dac73f41f75719c2
massiinisahaya <biwalake2018@gmail.com>
2024-12-02 14:19:10 +0900

    submit week1_homework

---
+++ a-4.py
@@ -0,0 +1,5 @@
+members = ["Bob", "Tom", "Ken"]
+
+# ここにコードを記述
+print(members[0])
+print(members[1])
---
+++ a-7.py
@@ -0,0 +1,5 @@
+even_numbers = [2, 4, 6, 8]
+
+# ここにコードを記述
+for number in even_numbers:
+    print(number * 2)
