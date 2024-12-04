78d4a680cca00e5bd3b697c5ee40fdb6cd1feceb
massiinisahaya <biwalake2018@gmail.com>
2024-12-04 14:23:13 +0900

    week1_homework C ouyou

--- d-6.py
+++ d-6.py
@@ -3,13 +3,13 @@ class MyCounterV3:
     def __init__(self, value, step):
         self.value = value
         self.step = step
-    
+
     def count_up(self):
         self.value += self.step
-    
+
     def count_down(self):
         self.value -= self.step
-        
+
 
 counter1 = MyCounterV3(value=1, step=2)
 print(counter1.value)  # 1
